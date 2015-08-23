9cb6df842717a28d8254f226d415ecdb25870357
Peter Kaleta <mail@peterkaleta.com>
2015-08-23 21:30:44 +0200

    proper order in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
-./node_modules/.bin/bower install
+#!/bin/bash
+export $( cat .env-production|xargs)
 npm install --ignore-scripts
+./node_modules/.bin/bower install
 cp sample.env .env
 cp sample.env-production .env-production
