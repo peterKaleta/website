f91f3db49abd854e07eb49f5b5a654007e406389
Peter Kaleta <mail@peterkaleta.com>
2015-08-23 16:54:00 +0200

    optimize for heroku

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-node dist/server/app.js
+export $( cat .env|xargs)
+./node_modules/nodemon/bin/nodemon.js \
+  --exec node_modules/.bin/babel-node \
+    --stage 0 \
+    -- ./server/app.js
