4d3e8dfd8bed3f280c942654dc326364460a0e4c
Peter Kaleta <mail@peterkaleta.com>
2015-08-23 21:52:24 +0200

    add webpack to deps

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export $( cat .env-production|xargs)
-npm install && ./node_modules/.bin/bower install
+npm install
+./node_modules/.bin/bower install
 cp sample.env .env
 cp sample.env-production .env-production
