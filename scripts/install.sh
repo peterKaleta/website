b3025501c50d2c808ec974873e706b7e1118e41a
Peter Kaleta <mail@peterkaleta.com>
2015-08-23 21:44:29 +0200

    make install pass on heroku:

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 export $( cat .env-production|xargs)
-
-npm install --ignore-scripts
-./node_modules/.bin/bower install
-
+npm install && ./node_modules/.bin/bower install
 cp sample.env .env
 cp sample.env-production .env-production
